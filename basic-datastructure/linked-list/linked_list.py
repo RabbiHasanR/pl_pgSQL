cf7126069a7c5edacdfe6cb8991d9ae90bc7bf18
rabbi <jasrabbi50@gmail.com>
2025-09-30 15:15:18 +0600

    singly linked list make from scratch done

--- basic-datastructure/linked-list/linked_list.py
+++ basic-datastructure/linked-list/linked_list.py
@@ -34,10 +34,82 @@ class LinkedList:
 
     
     def pop_front(self):
-        pass
+        if self.head:
+            val = self.head.val
+            self.head = self.head.next
+            return val
+        return None
     
     def pop_back(self):
-        pass
+        # if tail node track
+        # if self.head:
+        #     if self.head == self.tail:
+        #         val = self.head.val
+        #         self.head = None
+        #         self.tail = None
+        #         return val
+            
+        #     temp = self.head
+        #     while temp.next != self.tail:
+        #         temp = temp.next
+        #     val = self.tail.val
+        #     temp.next = None
+        #     self.tail = temp
+        #     return val
+        # return None
+        
+        
+        # if tail node not track
+        if not self.head:
+            return None
+        if not self.head.next:
+            val = self.head.val
+            self.head = None
+            return val
+        
+        prev = self.head
+        curr = self.head.next
+        
+        while curr.next:
+            prev = curr
+            curr = curr.next
+        
+        val = curr.val
+        prev.next = None
+        return val
+    
+    
+    def insert(self, val, pos):
+        if pos < 0:
+            return
+        if pos-1 == 0:
+            self.push_front(val)
+        else:
+            node = Node(val)
+            i = 0
+            temp = self.head
+            while i < pos - 1 and temp.next:
+                temp = temp.next
+                i += 1
+            
+            node.next = temp.next
+            temp.next = node
+            
+            
+    def search(self, val):
+        if not self.head:
+            return False
+        
+        temp = self.head
+        
+        while temp:
+            if temp.val == val:
+                return True
+            temp = temp.next
+        
+        return False
+        
+            
     
     def print(self):
         temp = self.head
@@ -57,3 +129,20 @@ ll.push_back(3)
 ll.push_front(4)
 
 ll.print()
+
+print("pop front:", ll.pop_front())
+ll.print()
+print("pop back:", ll.pop_back())
+ll.print()
+print("pop back:", ll.pop_back())
+ll.print()
+print("pop back:", ll.pop_back())
+ll.print()
+
+ll.insert(1,1)
+ll.print()
+ll.insert(2,3)
+ll.print()
+ll.insert(3,2)
+ll.print()
+print('found:', ll.search(4))
