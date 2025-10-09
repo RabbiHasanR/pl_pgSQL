bc32d3a85d4018abb76fd8e46cb12805cff7d32d
rabbi <jasrabbi50@gmail.com>
2025-10-09 17:01:03 +0600

    graph cycling done

--- basic-datastructure/graph/basic-of-graph.md
+++ basic-datastructure/graph/basic-of-graph.md
@@ -15,12 +15,16 @@ applications of graphs:
 graphs algorithms:
 shortest path
 shortest cyclic path
+minimum spanning tree(mst)
 
 
 vertex: every node of graph is a vertex
 edge: edge is a connection between two vertex
 uni-directional edge:
 bi-directional/undirectional edge:
+
+self edge
+
 
 directed graph
 undirected graph
@@ -29,9 +33,36 @@ edge based on weight
 weighted graph
 unweighted graph 
 
+disconected componets graph
+
 
 storing a graph:
-adjacency list:list of lists like [[(0,2)], [(1,2),(1,3)],[(2,0),(2,1),(2,3)],[(3,1),(3,2)]]
-adjacency matrix
-edge list
-2d matrix(implicit graph)
+adjacency list:list of lists like [[(0,2)], [(1,2),(1,3)],[(2,0),(2,1),(2,3)],[(3,1),(3,2)]]. for find neighbours from adjacency list time O(x)  x= total neighbours
+adjacency matrix:  for find neighbours from adjacency matrix time O(v)  v= total number of vertex
+edge list: only edges list like [[0,2],[1,2],[1,3],[2,3]]. list length is number of edges. using edge list can we finst MST
+2d matrix(implicit graph):
+
+
+
+graph traversals:
+
+breadth first search(BFS), indirecty level order traversal
+
+Depth first search(DFS)
+
+cycling:
+directed graph cycle
+undirected graph cycle
+
+
+topological sorting:
+work for only directed acyclic graph(DAG)
+
+Directed acyclic graph(DAG) is a directed graph with no cycles. topological sorting is used only for DAGs (not for non DAGs)
+It is a linear order of vertices such that every directed edge u -> v, the vertex u comes befor v in the order.
+
+
+
+undirected graph cycle detection technique: DFS, BFS, DSU(Disjoint Set Union)
+
+directed path cycle detection technique: DFS,BFS, Topological sorting, graphcolory
---
+++ basic-datastructure/graph/graph.py
@@ -0,0 +1,163 @@
+from collections import deque
+
+class Edge:
+    def __init__(self, src, dest, weight=0):
+        self.src = src
+        self.dest = dest
+        self.weight = weight
+        
+        
+def add_edge(graph, edge):
+    graph[edge.src].append(edge)
+    
+def print_vertex_neighbours(graph, vertex):
+    if vertex >= len(graph):
+        return
+    for edge in graph[vertex]:
+        print(f"neighbour:{edge.dest}, weight: {edge.weight}")
+        
+    print("\n")
+    
+    
+def print_all_vertex_with_neighbours(graph):
+    for i in range(len(graph)):
+        print(f"Vertex {i} has edges to:", [f"neighbour:{edge.dest}, weight: {edge.weight}" for edge in graph[i]])
+    
+
+def bfs(graph):
+    q = deque()
+    q.append(graph[0][0].src)
+    visited = [False] * len(graph)
+    while q:
+        v = q.popleft()
+        if not visited[v]:
+            print(v, end=" ")
+            visited[v] = True
+            for edge in graph[v]:
+                q.append(edge.dest)
+    print()
+    
+def dfs(graph, start, visited):
+    print(start, end=" ")
+    visited[start] = True
+    for edge in graph[start]:
+        v = edge.dest
+        if not visited[v]:
+            dfs(graph, v, visited)
+            
+
+def printAllPath(graph, start, visited, path, tar):
+    visited[start] = True
+    if start == tar:
+        print(path)
+        return
+    for edge in graph[start]:
+        v = edge.dest
+        if not visited[v]:
+            # visited[v] = True
+            printAllPath(graph, v, visited, path + str(v), tar)
+            visited[v] = False
+    # visited[start] = False
+             
+                
+    
+def create_undirected_graph(v=4):
+    graph = [[] for _ in range(v)]
+    add_edge(graph, Edge(0,2,2))
+    # add_edge(graph, Edge(1,3,0))
+    add_edge(graph, Edge(1,2,10))
+    add_edge(graph, Edge(2,0,2))
+    add_edge(graph, Edge(2,1,10))
+    add_edge(graph, Edge(2,3,-1))
+    # add_edge(graph, Edge(3,1,0))
+    add_edge(graph, Edge(3,2,-1))
+    return graph
+
+def create_directed_graph(v=4):
+    graph = [[] for _ in range(v)]
+    add_edge(graph, Edge(0,2))
+    add_edge(graph, Edge(1,0))
+    add_edge(graph, Edge(2,3))
+    # add_edge(graph, Edge(3,0))
+    return graph
+
+
+def isCycleDirected(graph, vis, curr, rec):
+    vis[curr] = True
+    rec[curr] = True
+    
+    for edge in graph[curr]:
+        if rec[edge.dest]:
+            return True
+        elif not vis[edge.dest]:
+            if isCycleDirected(graph, vis, edge.dest, rec):
+                return True
+    rec[curr] = False
+    return False
+
+def topologicalSortDirected(graph, curr, vis, stack):
+    vis[curr] = True
+    for edge in graph[curr]:
+        if not vis[edge.dest]:
+            topologicalSortDirected(graph, edge.dest, vis, stack)
+    stack.append(curr)
+    
+def topSort():
+    vis = [False] * 4
+    stack = []
+    for i in range(4):
+        if not vis[i]:
+            topologicalSortDirected(directed_graph, i, vis, stack)
+    return stack[::-1]
+
+
+def isCycleUndirected(graph, curr, vis, par):
+    vis[curr] = True
+    for v in graph[curr]:
+        if vis[v.dest] and par != v.dest:
+            return True
+        elif not vis[v.dest]:
+            if isCycleUndirected(graph, v.dest, vis, curr):
+                return True
+    return False
+
+graph = create_undirected_graph()
+print_vertex_neighbours(graph, 2)
+
+print("All vertex with edges:")
+
+print_all_vertex_with_neighbours(graph)
+
+print("Graph BFS Traversal")
+
+bfs(graph)
+
+print("Graph DFS Traversal")
+visited = [False] * len(graph)
+dfs(graph, 0, visited)
+print()
+
+print("All paths")
+visited = [False] * len(graph)
+start = 0
+tar = 1
+printAllPath(graph, start, visited, f"{start}", tar )
+
+vis = [False] * len(graph)
+
+print("is cycle undirected graph:", isCycleUndirected(graph, 0, vis, -1))
+
+# print(graph)
+
+directed_graph = create_directed_graph()
+vis = [False] * 4
+rec = [False] * 4
+
+print('is cycle directed graph:',isCycleDirected(directed_graph, vis, 0, rec))
+
+
+# topological short
+
+
+        
+print('topological short:', topSort())
